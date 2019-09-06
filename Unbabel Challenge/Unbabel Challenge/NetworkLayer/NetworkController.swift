2e94b38830a8ac752dcf6c6fb6f18af93c4f3669
Luís Rosa <luis.rosa@novabase.pt>
2019-09-06 15:45:08 +0100

    Added dependency injection to network controller

---
+++ Unbabel Challenge/Unbabel Challenge/NetworkLayer/NetworkController.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkController.swift
+//  Unbabel Challenge
+//
+//  Created by Luís Rosa on 06/09/2019.
+//  Copyright © 2019 Luís Rosa. All rights reserved.
+//
+
+import UIKit
+
+class NetworkController {
+
+    var network: NetworkInterface
+    
+    init(network: NetworkInterface) {
+        self.network = network
+    } 
+}
--- Unbabel Challenge/Unbabel Challenge/ViewController.swift
+++ Unbabel Challenge/Unbabel Challenge/ViewController.swift
@@ -9,13 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let networkController = NetworkController(network: NetworkCommunicator(baseUrl: "https://jsonplaceholder.typicode.com"))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let network: NetworkCommunicator = NetworkCommunicator(baseUrl: "https://jsonplaceholder.typicode.com")
-        
-        network.makeGETRequest(url: .posts) { (data, error) in
+        networkController.network.makeGETRequest(url: .posts) { (data, error) in
             
         }
     }
